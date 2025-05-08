0085f5b5e4bbb9d66c687bbde2a0f5768e45ed87
Lyrabyte <dvsloth@gmail.com>
2025-05-08 16:56:53 -0600

    forgor update serv
    forgor

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path    = require('path');
 const app     = express();
-const PORT    = process.env.PORT || 3522;
+const PORT    = process.env.PORT || 3521;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'MH-Web')));
 
 app.listen(PORT, () =>
   console.log(`Server running → http://localhost:${PORT}`)
